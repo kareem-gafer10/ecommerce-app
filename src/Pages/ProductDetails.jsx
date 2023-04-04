35b4aee47d21c03f29f19e9002748b1b01ca7da4
kareem-gafer10 <kareemgafer28.com>
2023-04-04 20:55:06 +0200

    edit product details

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -37,7 +37,7 @@ if (!product) {
 
 
            <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
-              <img className='max-w-[200px] lg:max-w-sm' src={image} alt={title} />
+              <img className='max-w-[150px] lg:max-w-sm' src={image} alt={title} />
            </div>
 
          <div className='flex-1 text-center lg:text-left'>
